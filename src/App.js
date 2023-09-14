e7002531d4843b99f80e3bb04baf59b2538b80db
Faisallm <faisallawan1997@gmail.com>
2023-09-14 13:59:16 -0700

    coder coder fruit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+}
+
+function ImageTile() {
+  return <div className="tile"></div>;
+}
+
+function Imager(imagurl) {
+  return <img src={imagurl} />;
+}
+
+function AdminPanel() {
+  return <div>Admin Panel</div>;
+}
+
+function LoginForm() {
+  return <div>LoginForm</div>;
+}
+
+export default function MyApp() {
+  // we want this button component to
+  // remember all the time it was
+  // clicked.
+
+  // current state: count
+  // function that allows us to setCount
+
+  const [count, setCount] = useState(0);
+
+  function clickHandler() {
+    setCount(count + 1);
+  }
+
+  const user = {
+    name: "Faisal",
+    wife: "Amina",
+    imgUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  };
+
+  // list of objects
+  const fruits = [
+    { title: "Cabbage", isFruit: false, id: 1 },
+    { title: "Garlic", isFruit: false, id: 2 },
+    { title: "Apple", isFruit: true, id: 3 },
+  ];
+
+  const isLoggedIn = false;
+
+  const fruitList = fruits.map((fruit) => {
+    return (
+      <li
+        key={fruit.id}
+        style={{
+          // setting color based on a condition
+          color: fruit.isFruit ? "magenta" : "darkgreen",
+        }}
+      >
+        {fruit.title}
+      </li>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my App, {user.name}</h1>
+      <MyButton count={count} onClick={clickHandler} />
+      <MyButton count={count} onClick={clickHandler} />
+      <ImageTile />
+      {isLoggedIn && <AdminPanel />}
+      <ul>{fruitList}</ul>
     </div>
   );
 }
-
-export default App;
